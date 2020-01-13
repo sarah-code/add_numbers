9e17af3c5290c126588f04315df97e5be958c61f
sarah-code <trancansar@outlook.com>
2020-01-13 11:39:38 +0100

    Corrected handling of exit condition and recursion.

--- src/com/company/Numbers.java
+++ src/com/company/Numbers.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Numbers {
@@ -7,19 +8,25 @@ public class Numbers {
     Scanner scan = new Scanner(System.in);
     public void getNumber()
     {
-        scan = new Scanner(System.in);
-        System.out.print("Any letter key will abort this program) Please enter a number: ");
-        int num = scan.nextInt();
-        if (String.valueOf(num).matches("[a-zA-Z]+") == true)
+        int num = 0;
+        try {
+            scan = new Scanner(System.in);
+            System.out.print("(Any letter key will abort this program) Please enter a number: ");
+            num = scan.nextInt();
+        }
+        catch(InputMismatchException exception)
         {
+            System.err.println(exception +" encountered.");
+            System.out.println("Exiting programme now. / Termination du logiciel maintement.");
             System.exit(-1);
         }
         this.number = num;
+
         menu(number);
     }
     public void menu(int num)
     {
-        System.out.println("Please select how to treat the number: \n (s) to summarize all digits. \n (p) to multiplicate all digits. \n any other key to abort.");
+        System.out.println("Please select how to treat the number: \n (s) to summarize all digits. \n (p) to multiplicate all digits. \n (m) minus all digits. \n (d) divide all digits. \n any other key to exit the program.");
         String descision = scan.next();
 
         switch(descision)
@@ -27,63 +34,169 @@ public class Numbers {
             case ("s"):
                 sum(num); break;
             case("p"):
-                product(num); break;
+            product(num); break;
+            case("m"):
+                minus(num); break;
+            case("d"):
+                division(num); break;
             default:
+                System.out.println("Exiting programme now. / Termination du logiciel maintement.");
                 System.exit(-1);
         }
     }
     public void sum(int num)
     {
-        int length = (int)(Math.log10(num)+1);
-        int  splitnum = num;
-        int split = 0;
-        int count = 0;
-        for(int i = length ; i > 0; i--)
+        int length =  returnLength(num);
+        while (length > 0)
         {
-            split = splitnum %10;
-            System.out.print(split);
+            int splitnum = num;
+            int split = 0;
+            int count = 0;
+            for(int i = length ; i > 0; i--)
+            {
+                split = splitnum %10;
+                System.out.print(split);
 
 
-            splitnum = splitnum / 10;
-            count = count + split;
-            if (i > 1)
-            {
-                System.out.print(" + ");
+                splitnum = splitnum / 10;
+                count = count + split;
+                if (i > 1)
+                {
+                    System.out.print(" + ");
+                }
+                else
+                {
+                    System.out.println(" = " +count);
+                }
             }
-            else
+
+            System.out.println("The sum of the given int "+num+" is: "+ count );
+
+
+            if (returnLength(count) == 1)
             {
-                System.out.println(" = " +count);
+                getNumber();
             }
+            sum(count);
         }
-
-        System.out.println("The sum of the given int "+num+" is: "+ count );
-        getNumber();
     }
+
+    public void minus(int num)
+    {
+        int length =  returnLength(num);
+        while (length > 0)
+        {
+            int splitnum = num;
+            int split = 0;
+            int count = 0;
+            for(int i = length ; i > 0; i--)
+            {
+                split = splitnum %10;
+                System.out.print(split);
+
+
+                splitnum = splitnum / 10;
+                count = count - split;
+                if (i > 1)
+                {
+                    System.out.print(" - ");
+                }
+                else
+                {
+                    System.out.println(" = " +count);
+                }
+            }
+
+            System.out.println("The sum of the given int "+num+" is: "+ count );
+
+
+            if (returnLength(count) == 1 || count <0)
+            {
+                getNumber();
+            }
+            minus(count);
+        }
+    }
+    public void division(int num)
+    {
+        int length =  returnLength(num);
+        while (length > 0)
+        {
+
+            int  splitnum = num;
+            int split = 0;
+            int count =1;
+            for(int i = length ; i > 0; i--)
+            {
+                split = splitnum %10;
+                System.out.print(split);
+
+
+                splitnum = splitnum / 10;
+                count = count / split;
+                if (i > 1)
+                {
+                    System.out.print(" / ");
+                }
+                else
+                {
+                    System.out.println(" = " +count);
+                }
+            }
+
+            System.out.println("The product of the given int "+num+" is: "+ count );
+
+
+            if (returnLength(count) == 1 || count == 0)
+            {
+                getNumber();
+            }
+            division(count);
+        }
+    }
+
+
+
     public void product(int num)
     {
-        int length = (int)(Math.log10(num)+1);
-        int  splitnum = num;
-        int split = 0;
-        int count =1;
-        for(int i = length ; i > 0; i--)
+        int length =  returnLength(num);
+        while (length > 0)
         {
-            split = splitnum %10;
-            System.out.print(split);
+
+            int  splitnum = num;
+            int split = 0;
+            int count =1;
+            for(int i = length ; i > 0; i--)
+            {
+                split = splitnum %10;
+                System.out.print(split);
 
 
-            splitnum = splitnum / 10;
-            count = count * split;
-            if (i > 1)
-            {
-                System.out.print(" * ");
+                splitnum = splitnum / 10;
+                count = count * split;
+                if (i > 1)
+                {
+                    System.out.print(" * ");
+                }
+                else
+                {
+                    System.out.println(" = " +count);
+                }
             }
-            else
+
+            System.out.println("The product of the given int "+num+" is: "+ count );
+
+
+            if (returnLength(count) == 1 || count == 0)
             {
-                System.out.println(" = " +count);
+                getNumber();
             }
+            product(count);
         }
-
-        System.out.println("The product of the given int "+num+" is: "+ count );
-        getNumber();
+    }
+    public int returnLength(int num)
+    {
+        int len = (int)(Math.log10(num)+1);
+        return len;
     }
 }
