00b3f3ced227910211c48fa2668ff86db02b2f9c
sarah-code <trancansar@outlook.com>
2020-02-16 17:27:14 +0100

    readded subtraction mode (multplication mode is still buggy)

--- src/com/company/Numbers.java
+++ src/com/company/Numbers.java
@@ -4,7 +4,7 @@ import java.util.InputMismatchException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class Numbers {
+public class Numbers  {
     private int number;
     private String splitnum;
     private String split [];
@@ -31,7 +31,7 @@ public class Numbers {
     }
     public void menu(int num)
     {
-        System.out.println("Please select how to treat the number: \n (s) to summarize all digits. \n (p) to multiplicate all digits. \n any other key to exit the program.");
+        System.out.println("Please select how to treat the number: \n (s) to summarize all digits. \n (p) to multiplicate all digits. \n (m) to subtract all digits. \n any other key to exit the program.");
         String descision = scan.next();
 
         switch(descision)
@@ -40,8 +40,8 @@ public class Numbers {
                 sum(num); break;
             case("p"):
             product(num); break;
-//            case("m"):
-//                minus(num); break;
+            case("m"):
+                minus(num); break;
             default:
                 System.out.println("Exiting programme now. / Termination du logiciel maintement.");
                 System.exit(-1);
@@ -72,8 +72,9 @@ public class Numbers {
    public void product(int num)
     {
         String temp[] = splitNumber(num);
+        System.out.println(splitnum);
         int length = returnLength(num);
-        if (length  !=1 || num != 0){
+        if (length  !=1 || splitnum != null || splitnum != "0") {
             try {
                 int numberInt = 1;
                 for (int i = 0; i < temp.length; i++) {
@@ -97,51 +98,36 @@ public class Numbers {
 
                 System.out.println("The product of the given int " + num + " is: " + numberInt);
                 product(numberInt);
-            }catch(Exception e)
-            {
+            } catch (Exception e) {
                 getNumber();
+
             }
-        }else {
             getNumber();
         }
     }
-//    public void minus(int num)
-//    {
-//        int length =  returnLength(num);
-//        while (length > 0)
-//        {
-//            splitnum = num;
-//
-//            for(int i = length ; i > 0; i--)
-//            {
-//                split = splitnum %10;
-//                System.out.print(split);
-//
-//
-//                splitnum = splitnum / 10;
-//                count = count - split;
-//                if (i > 1)
-//                {
-//                    System.out.print(" - ");
-//                }
-//                else
-//                {
-//                    System.out.println(" = " +count);
-//                }
-//
-//            }
-//
-//            System.out.println("The sum of the given int "+num+" is: "+ count );
-//
-//
-//            if (returnLength(count) == 1 || count > -100)
-//            {
-//                getNumber();
-//            }
-//            sum(count);
-//        }
-//    }
-//
+    public void minus(int num)
+    {
+        int length = returnLength(num);
+        if(length  !=1 || num != -100) {
+            int numberInt = 0;
+            String temp[] = splitNumber(num);
+            for (int i = 0; i < temp.length; i++) {
+                System.out.print(temp[i]);
+                if (i != temp.length - 1) {
+                    System.out.print(" - ");
+                } else {
+                    System.out.print(" = ");
+                }
+                numberInt = numberInt - Integer.parseInt(temp[i]);
+            }
+            System.out.println(numberInt);
+
+            System.out.println("The sum of the given int " + num + " is: " + numberInt);
+            sum(numberInt);
+        }
+        getNumber();
+    }
+
     public int returnLength(int num)
     {
         int len = (int)(Math.log10(num)+1);
